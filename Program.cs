971e2767a0a468039da9d3a93a3c5e0bf3de6e9b
Rafiul Islam <rafiul.islam@ovgu.de>
2023-03-07 17:02:25 +0100

    first draft with sample property values

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using MqttClientSouthboundSimulatedvalues;
+
+using Newtonsoft.Json;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using uPLibrary.Networking.M2Mqtt;
+
+class Program
+{
+    
+    // Main Method
+   static MqttClient client ;
+    public int a;
+    
+
+    static public void Main(String[] args)
+    {
+      
+        // create client instance and connect to publicly hosted broker
+       client = new uPLibrary.Networking.M2Mqtt.MqttClient("test.mosquitto.org");
+       string clientId = Guid.NewGuid().ToString();
+      client.Connect(clientId);
+       String topic = "MacnineData/ID-0000";
+        Status sta = new Status();
+
+
+        RandomNumberGernarator rn = new RandomNumberGernarator();
+         
+       
+        while (true)
+        {
+            
+            double Tval = rn.Getrandomvalues(10,35);
+
+            string Temp = String.Format("{0:0.00}", Tval);
+
+            double Hval = rn.Getrandomvalueshumidity(22, 50);
+            String Humidity = String.Format("{0:0.00}", Hval);
+
+            double Spindlespeed = rn.GetSpindlespeed(0, 50);
+            String speed = String.Format("{0:0.00}", Spindlespeed);
+
+
+            String MachineStatus = sta.stringGetStatus();
+                
+            var jsonObject = new
+            {
+                Temperature = Temp,
+                Humidity = Humidity,
+                Speed = speed,
+                Timestamp = DateTime.Now.ToString(),
+                MachineStatus = MachineStatus
+            };
+
+            // convert JSON object to string
+            string data = JsonConvert.SerializeObject(jsonObject);
+
+            // print JSON string
+
+            Console.WriteLine("Temperature : " + Temp);
+            Console.WriteLine("Humidity : " + Humidity);
+            Console.WriteLine("Spindle Speed : " + speed);
+            Console.WriteLine("Timestamp : " + jsonObject.Timestamp);           
+            client.Publish(topic, System.Text.Encoding.UTF8.GetBytes(data));
+
+           Console.WriteLine("Data published to topic: " + topic + " with payload: " + data);
+           // Console.ReadLine();
+            System.Threading.Thread.Sleep(1000);  
+
+        }
+    }
+}
---
+++ Status.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Status
+{
+
+	public string status { get; set; } = string.Empty;
+	public int i { get; set; } = 0;
+
+	public string stringGetStatus()
+	{
+		if (i >= 0 && i < 200)
+		{
+			status = "on";
+			i = i + 1;
+			return status;
+		}
+		else if (i >= 200 && i <= 300)
+		{
+			status = "off";
+			i = i + 1;
+			if(i == 300)
+			{
+				i = 0;
+			}
+            return status;
+        }
+		return string.Empty;
+	}
+}
+		
+	
+
